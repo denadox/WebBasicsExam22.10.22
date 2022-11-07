601ec4e519789495d95b31542b6c9b3257af32ed
Ivo Dimitrov <ivaylo.dimitrov93@gmail.com>
2022-11-08 00:58:13 +0100

    Project commited on github

---
+++ Library/Contracts/IBookService.cs
@@ -0,0 +1,20 @@
+﻿using Library.Data.Models;
+using Library.Models;
+
+namespace Library.Contracts
+{
+    public interface IBookService
+    {
+        Task<IEnumerable<BookViewModel>> GetAllAsync();
+
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+
+        Task AddBookAsync(AddBookViewModel model);
+
+        Task AddBookToCollectionAsync(int bookId, string applicationUserId);
+
+        Task<IEnumerable<BookViewModel>> GetReadAsync(string applicationUserId);
+
+        Task RemoveBookFromCollectionAsync(int bookId, string applicationUserId);
+    }
+}
